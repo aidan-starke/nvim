3330fcab1b0b87f799240f035f38d72e6d1e8297
aidan-starke <starke.aidan@gmail.com>
2022-10-25 08:15:08 +1300

    `h` for Harpoon

--- after/plugin/harpoon.rc.lua
+++ after/plugin/harpoon.rc.lua
@@ -23,8 +23,12 @@ harpoon.setup({
 
 local mapOpts = { noremap = true, silent = true }
 
-vim.api.nvim_set_keymap("n", "<leader>mx", "<cmd>lua require('harpoon.mark').add_file()<cr>", mapOpts)
-vim.api.nvim_set_keymap("n", "<leader>mm", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", mapOpts)
-vim.api.nvim_set_keymap("n", "<leader>mn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", mapOpts)
-vim.api.nvim_set_keymap("n", "<leader>mp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", mapOpts)
-vim.api.nvim_set_keymap("n", "<leader>mt", "<cmd>Telescope harpoon marks<cr>", mapOpts)
+vim.api.nvim_set_keymap("n", "<leader>hx", "<cmd>lua require('harpoon.mark').add_file()<cr>", mapOpts)
+vim.api.nvim_set_keymap("n", "<leader>hm", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", mapOpts)
+vim.api.nvim_set_keymap("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", mapOpts)
+vim.api.nvim_set_keymap("n", "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", mapOpts)
+vim.api.nvim_set_keymap("n", "<leader>ht", "<cmd>Telescope harpoon marks<cr>", mapOpts)
+vim.api.nvim_set_keymap("n", "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1) <cr>", mapOpts)
+vim.api.nvim_set_keymap("n", "<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(2) <cr>", mapOpts)
+vim.api.nvim_set_keymap("n", "<leader>h3", "<cmd>lua require('harpoon.ui').nav_file(3) <cr>", mapOpts)
+vim.api.nvim_set_keymap("n", "<leader>h4", "<cmd>lua require('harpoon.ui').nav_file(4) <cr>", mapOpts)
