7b5a7a68f8496428626f386682cb196422ce90f9
aidan-starke <starke.aidan@gmail.com>
2022-10-28 05:39:02 +1300

    Limit tailwindcss filetypes

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -134,7 +134,8 @@ nvim_lsp.sumneko_lua.setup {
 
 nvim_lsp.tailwindcss.setup {
 	on_attach = on_attach,
-	capabilities = capabilities
+	capabilities = capabilities,
+	filetypes = { "javascriptreact", "typescriptreact", "svelte" }
 }
 
 nvim_lsp.solc.setup {
