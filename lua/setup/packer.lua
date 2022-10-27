ebb0b92ccdfbd16ef8a83675daa92be4d97ef4dc
aidan-starke <starke.aidan@gmail.com>
2022-10-28 05:47:04 +1300

    Remove unnecessary `typescript` plugin

--- lua/setup/packer.lua
+++ lua/setup/packer.lua
@@ -17,7 +17,6 @@ return require('packer').startup(function(use)
 	use 'glepnir/lspsaga.nvim' -- LSP UIs
 	use 'simrat39/rust-tools.nvim' -- Rust tools
 	use 'j-hui/fidget.nvim' -- Visual LSP progress
-	use 'jose-elias-alvarez/typescript.nvim' -- Typescript support
 	use 'L3MON4D3/LuaSnip' -- Snippets
 	use 'wellle/context.vim' -- Keep function definition visible
 	use 'kdheepak/lazygit.nvim' -- Visual git integration
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -266,11 +266,6 @@ _G.packer_plugins = {
     path = "/Users/aidan/.local/share/nvim/site/pack/packer/start/toggleterm.nvim",
     url = "https://github.com/akinsho/toggleterm.nvim"
   },
-  ["typescript.nvim"] = {
-    loaded = true,
-    path = "/Users/aidan/.local/share/nvim/site/pack/packer/start/typescript.nvim",
-    url = "https://github.com/jose-elias-alvarez/typescript.nvim"
-  },
   ["vim-illuminate"] = {
     loaded = true,
     path = "/Users/aidan/.local/share/nvim/site/pack/packer/start/vim-illuminate",
