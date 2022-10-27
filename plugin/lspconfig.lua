235891756ad55f62937f4780a116483e6ff8af00
aidan-starke <starke.aidan@gmail.com>
2022-10-28 06:35:33 +1300

    Refactor

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -42,11 +42,11 @@ local on_attach = function(client, bufnr)
 	-- Aerial window
 	buf_set_keymap('n', '<leader>;a', '<cmd>AerialToggle!<CR>', opts)
 	-- Jump forwards/backwards with '{' and '}'
-	buf_set_keymap('n', '{', '<cmd>AerialPrev<CR>', opts)
-	buf_set_keymap('n', '}', '<cmd>AerialNext<CR>', opts)
+	buf_set_keymap('n', '{', '<cmd>AerialNext<CR>', opts)
+	buf_set_keymap('n', '}', '<cmd>AerialPrev<CR>', opts)
 	-- Jump up the tree with '[[' or ']]'
-	buf_set_keymap('n', '[[', '<cmd>AerialPrevUp<CR>', opts)
-	buf_set_keymap('n', ']]', '<cmd>AerialNextUp<CR>', opts)
+	buf_set_keymap('n', ']]', '<cmd>lua require("aerial").next_up()<CR>', opts)
+	buf_set_keymap('n', '[[', '<cmd>lua require("aerial").prev_up()<CR>', opts)
 
 	-- Diagnostic hover
 	vim.api.nvim_create_autocmd("CursorHold", {
@@ -98,52 +98,60 @@ protocol.CompletionItemKind = {
 -- Set up completion using nvim_cmp with LSP source
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-nvim_lsp.flow.setup {
-	on_attach = on_attach,
-	capabilities = capabilities
+local servers = { "flow",
+	"tailwindcss",
+	"sumneko_lua",
+	"tsserver",
+	"solc"
 }
 
-nvim_lsp.tsserver.setup {
-	on_attach = on_attach,
-	filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-	cmd = { "typescript-language-server", "--stdio" },
-	capabilities = capabilities,
-	root_dir = function() return vim.loop.cwd() end
-}
+local opts = {}
 
-nvim_lsp.sumneko_lua.setup {
-	capabilities = capabilities,
-	on_attach = function(client, bufnr)
-		on_attach(client, bufnr)
-		enable_format_on_save(client, bufnr)
-	end,
-	settings = {
-		Lua = {
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { 'vim' },
+for _, lsp in ipairs(servers) do
+	opts = {
+		on_attach = on_attach,
+		capabilities = capabilities,
+	}
+	if lsp == "sumneko_lua" then
+		local lua_opts = {
+			on_attach = function(client, bufnr)
+				on_attach(client, bufnr)
+				enable_format_on_save(client, bufnr)
+			end,
+			settings = {
+				Lua = {
+					diagnostics = {
+						-- Get the language server to recognize the `vim` global
+						globals = { 'vim' },
+					},
+
+					workspace = {
+						-- Make the server aware of Neovim runtime files
+						library = vim.api.nvim_get_runtime_file("", true),
+						checkThirdParty = false
+					},
+				},
 			},
+		}
+		opts = vim.tbl_deep_extend("force", opts, lua_opts)
+	end
 
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false
-			},
-		},
-	},
-}
+	if lsp == "tailwindcss" then
+		local tailwind_opts = {
+			filetypes = { "javascriptreact", "typescriptreact", "svelte" }
+		}
+		opts = vim.tbl_deep_extend("force", opts, tailwind_opts)
+	end
 
-nvim_lsp.tailwindcss.setup {
-	on_attach = on_attach,
-	capabilities = capabilities,
-	filetypes = { "javascriptreact", "typescriptreact", "svelte" }
-}
+	if lsp == "solc" then
+		local solc_opts = {
+			cmd = { "solcjs", "--base-path", vim.fn.getcwd() }
+		}
+		opts = vim.tbl_deep_extend("force", opts, solc_opts)
+	end
 
-nvim_lsp.solc.setup {
-	on_attach = on_attach,
-	capabilities = capabilities,
-	root_dir = nvim_lsp.util.root_pattern('hardhat.config.*', '.git'),
-}
+	nvim_lsp[lsp].setup(opts)
+end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 	vim.lsp.diagnostic.on_publish_diagnostics, {
@@ -153,6 +161,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 	severity_sort = true,
 }
 )
+
 -- Diagnostic symbols in the sign column (gutter)
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
