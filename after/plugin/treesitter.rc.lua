a1a6b0e75d367964606cbb915c305ee76117c64e
aidan-starke <starke.aidan@gmail.com>
2022-11-04 08:43:14 +1300

    Ensure treesitter installs

--- after/plugin/treesitter.rc.lua
+++ after/plugin/treesitter.rc.lua
@@ -1,6 +1,15 @@
 local status, ts = pcall(require, "nvim-treesitter.configs")
 if (not status) then return end
 ts.setup {
+	ensure_installed = {
+		"lua",
+		"javascript",
+		"typescript",
+		"solidity",
+		"svelte",
+		"rust",
+		"graphql"
+	},
 	highlight = {
 		enable = true,
 		disable = {},
@@ -11,7 +20,7 @@ ts.setup {
 	},
 	autotag = {
 		enable = true,
-	},
+	}
 }
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
