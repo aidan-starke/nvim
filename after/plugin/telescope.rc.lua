070c6c2cfe72c7c94267681a9369af683922334f
aidan-starke <starke.aidan@gmail.com>
2022-11-11 19:27:14 +1300

    Use ivy theme but ignore `.git/` to make search BLAZINGLY FAST

--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -9,6 +9,11 @@ telescope.setup {
 			},
 		},
 	},
+	pickers = {
+		find_files = {
+			find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" },
+		}
+	},
 	extensions = {
 		fzf = {
 			fuzzy = true, -- false will only do exact matching
@@ -25,35 +30,36 @@ telescope.load_extension("fzf")
 
 local nmap = require("setup.keymap").nmap
 local builtin = require("telescope.builtin")
+local ivy_theme = require("telescope.themes").get_ivy()
+local merge_tables = require("setup.helpers").merge_tables
 
 require("setup.helpers").set_keymaps(nmap, {
 	{ ';f',
 		function()
-			builtin.find_files({
-				no_ignore = true,
+			builtin.find_files(merge_tables({
 				hidden = true
-			})
+			}, ivy_theme))
 		end },
 	{ ';g', function()
-		builtin.live_grep()
+		builtin.live_grep(ivy_theme)
 	end },
 	{ '\\\\', function()
-		builtin.buffers()
+		builtin.buffers(ivy_theme)
 	end },
 	{ ';t', function()
-		builtin.help_tags()
+		builtin.help_tags(ivy_theme)
 	end },
 	{ ';;', function()
-		builtin.resume()
+		builtin.resume(ivy_theme)
 	end },
 	{ ';d', function()
-		builtin.diagnostics()
+		builtin.diagnostics(ivy_theme)
 	end },
 	{ ';z', function()
-		builtin.current_buffer_fuzzy_find()
+		builtin.current_buffer_fuzzy_find(ivy_theme)
 	end },
 	{ ";b", function()
-		telescope.extensions.file_browser.file_browser({
+		telescope.extensions.file_browser.file_browser(merge_tables({
 			path = "%:p:h",
 			cwd = vim.fn.expand('%:p:h'),
 			respect_gitignore = false,
@@ -62,24 +68,24 @@ require("setup.helpers").set_keymaps(nmap, {
 			previewer = false,
 			initial_mode = "normal",
 			layout_config = { height = 40 }
-		})
+		}, ivy_theme))
 	end },
 	{ ";h", function()
-		telescope.extensions.harpoon.marks()
+		telescope.extensions.harpoon.marks(ivy_theme)
 	end },
 	{ ';db', function()
-		telescope.extensions.dap.list_breakpoints()
+		telescope.extensions.dap.list_breakpoints(ivy_theme)
 	end },
 	{ ';dc', function()
-		telescope.extensions.dap.commands()
+		telescope.extensions.dap.commands(ivy_theme)
 	end },
 	{ ';dv', function()
-		telescope.extensions.dap.variables()
+		telescope.extensions.dap.variables(ivy_theme)
 	end },
 	{ ';df', function()
-		telescope.extensions.dap.frames()
+		telescope.extensions.dap.frames(ivy_theme)
 	end },
 	{ ';c', function()
-		telescope.load_extension('neoclip').default()
+		telescope.load_extension('neoclip').default(ivy_theme)
 	end }
 })
