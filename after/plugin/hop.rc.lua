5a8c4f659dbffa5727ba49875efa49f45dc9353f
aidan-starke <starke.aidan@gmail.com>
2022-11-13 20:33:10 +1300

    Prefix `hop` keymaps with `<leader>`

--- after/plugin/hop.rc.lua
+++ after/plugin/hop.rc.lua
@@ -7,16 +7,28 @@ local directions = require('hop.hint').HintDirection
 local set_keymaps = require("setup.helpers").set_keymaps
 
 set_keymaps(require("setup.keymap").remap, {
-	{ 'f', function()
-		hop.hint_char1({ direction = directions.AFTER_CURSOR, })
-	end, },
-	{ 'F', function()
-		hop.hint_char1({ direction = directions.BEFORE_CURSOR, })
-	end, },
-	{ 't', function()
-		hop.hint_char1({ direction = directions.AFTER_CURSOR, hint_offset = -1 })
-	end, },
-	{ 'T', function()
-		hop.hint_char1({ direction = directions.BEFORE_CURSOR, hint_offset = -1 })
-	end, }
+	{
+		'<leader>f',
+		function()
+			hop.hint_char1({ direction = directions.AFTER_CURSOR, })
+		end,
+	},
+	{
+		'<leader>F',
+		function()
+			hop.hint_char1({ direction = directions.BEFORE_CURSOR, })
+		end,
+	},
+	{
+		'<leader>t',
+		function()
+			hop.hint_char1({ direction = directions.AFTER_CURSOR, hint_offset = -1 })
+		end,
+	},
+	{
+		'<leader>T',
+		function()
+			hop.hint_char1({ direction = directions.BEFORE_CURSOR, hint_offset = -1 })
+		end,
+	}
 })
