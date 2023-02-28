027c815e83fbcee4cc50f0492e473ad2cfa5c4ba
aidan-starke <starke.aidan@gmail.com>
2023-02-28 17:35:48 +1300

    Add startup autocmd

--- after/plugin/nvim-tree.rc.lua
+++ after/plugin/nvim-tree.rc.lua
@@ -28,3 +28,19 @@ tree.setup({
 		ignore = false,
 	},
 })
+
+-- open on startup
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = function(data)
+	-- buffer is a directory
+	local directory = vim.fn.isdirectory(data.file) == 1
+
+	if not directory then
+		return
+	end
+
+	-- change to the directory
+	vim.cmd.cd(data.file)
+
+	-- open the tree
+	require("nvim-tree.api").tree.open()
+end })
