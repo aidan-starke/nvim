ab613ff5c0004381c708d8f92f5e36257b39684f
aidan-starke <starke.aidan@gmail.com>
2023-05-07 15:57:34 +1200

    Update plugins

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -21,6 +21,7 @@ end
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
+---@diagnostic disable-next-line: unused-local
 local on_attach = function(client, bufnr)
 	local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, 'n', ...) end
 
