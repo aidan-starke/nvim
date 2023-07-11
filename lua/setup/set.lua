15e51027446013cb5493b91db88494d783363b3a
Aidan <starke.aidan@gmail.com>
2023-07-11 16:35:00 +1200

    Update copilot node command

--- lua/setup/set.lua
+++ lua/setup/set.lua
@@ -25,4 +25,4 @@ vim.opt.updatetime = 50
 vim.opt.wildmenu = true
 vim.opt.wildmode = { 'longest', 'full', 'full' }
 
-vim.g.copilot_node_command = '/usr/local/Cellar/node@16/16.20.0/bin/node'
+vim.g.copilot_node_command = '/opt/homebrew/Cellar/node@16/16.20.1_1/bin/node'
