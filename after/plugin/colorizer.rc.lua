9ca21b497f66cba2721ec12200fc5cacb846ce42
Aidan <starke.aidan@gmail.com>
2023-07-31 19:58:00 +1200

    Set termguicolors in `set.lua`

--- after/plugin/colorizer.rc.lua
+++ after/plugin/colorizer.rc.lua
@@ -1,8 +1,6 @@
 local status, colorizer = pcall(require, "colorizer")
 if (not status) then return end
 
-vim.cmd [[set termguicolors]] -- enable 24-bit RGB colors
-
 colorizer.setup({
-	'*';
+	'*',
 })
