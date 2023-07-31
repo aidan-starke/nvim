95ba9dd01965bb09600ad6fc8b461c2fe8c67b23
Aidan <starke.aidan@gmail.com>
2023-07-31 19:55:53 +1200

    Set `relativenumber` true for `NvimTree`

--- after/plugin/nvim-tree.rc.lua
+++ after/plugin/nvim-tree.rc.lua
@@ -27,20 +27,25 @@ tree.setup({
 	git = {
 		ignore = false,
 	},
+	view = {
+		relativenumber = true
+	}
 })
 
 -- open on startup
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = function(data)
-	-- buffer is a directory
-	local directory = vim.fn.isdirectory(data.file) == 1
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+	callback = function(data)
+		-- buffer is a directory
+		local directory = vim.fn.isdirectory(data.file) == 1
 
-	if not directory then
-		return
+		if not directory then
+			return
+		end
+
+		-- change to the directory
+		vim.cmd.cd(data.file)
+
+		-- open the tree
+		require("nvim-tree.api").tree.open()
 	end
-
-	-- change to the directory
-	vim.cmd.cd(data.file)
-
-	-- open the tree
-	require("nvim-tree.api").tree.open()
-end })
+})
