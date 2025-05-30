9e14c34df3bfb182d57ecc9b93c89df59f78f9c7
Aidan <starke.aidan@gmail.com>
2025-05-30 16:48:35 +1200

    harpoon2

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -7,11 +7,14 @@ harpoon.setup({
 	excluded_filetypes = { "harpoon", "NvimTree", "TelescopePrompt" },
 })
 
+local harpoon_extensions = require("harpoon.extensions")
+harpoon:extend(harpoon_extensions.builtins.highlight_current_file())
+
 local set_keymaps, nnoremap = require("setup.helpers").set_keymaps, require("setup.keymap").nnoremap
 
 set_keymaps(nnoremap, {
-	{ "<leader>hx", "<cmd>lua require('harpoon.mark').add_file()<cr>" },
-	{ "<leader>hm", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>" },
-	{ "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>" },
-	{ "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>" },
+	{ "<leader>hx", function() harpoon:list():add() end },
+	{ "<leader>hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end },
+	{ "<leader>hn", function() harpoon:list():next({ ui_nav_wrap = true }) end },
+	{ "<leader>hp", function() harpoon:list():prev() end },
 }, { silent = true })
